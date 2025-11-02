086efead6916993718c85c27709454761dae3a97
Antonio-Conrado <jconradolopez19@gmail.com>
2025-11-01 18:13:47 -0600

    updated enum

--- lib/enum.ts
+++ lib/enum.ts
@@ -3,3 +3,39 @@ export enum ROLE_ENUM {
   TRAINER = "Entrenador",
   ADMIN = "Administrador",
 }
+
+export enum DAYS_OF_WEEK {
+  MONDAY = "Lunes",
+  TUESDAY = "Martes",
+  WEDNESDAY = "Miércoles",
+  THURSDAY = "Jueves",
+  FRIDAY = "Viernes",
+  SATURDAY = "Sábado",
+  SUNDAY = "Domingo",
+}
+
+export enum STATUS_LIST {
+  PENDING = "Pendiente",
+  CONFIRMED = "Confirmado",
+  CANCELLED = "Cancelado",
+  MISSED = "No asistido",
+  COMPLETED = "Completado",
+  RESCHEDULED = "Reprogramado",
+}
+
+export enum CONCEPT {
+  DAY = "Pase diario",
+  WEEK = "Pase semanal",
+  MONTH = "Membresía mensual",
+  FORTNIGHT = "Membresía quincenal",
+  QUARTER = "Membresía trimestral",
+  SEMESTER = "Membresía semestral",
+  YEAR = "Membresía anual",
+  OTHER = "Otro",
+}
+
+export enum PAYMENT_METHOD {
+  CASH = "Efectivo",
+  CARD = "Tarjeta",
+  TRANSFER = "Transferencia",
+}
