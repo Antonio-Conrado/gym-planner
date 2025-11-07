1977d95fdefe9db2e071953b833f02c1ade46bb0
Antonio-Conrado <jconradolopez19@gmail.com>
2025-11-07 10:47:06 -0600

    Added ErrorAlert component with optional redirect or link handling

--- shared/components/alert/ErrorAlert.tsx
+++ shared/components/alert/ErrorAlert.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
+import RedirectBack from "../nav/RedirectBack";
 
 type Props = {
   title: string;
   description: string;
   href?: string;
   linkValue?: string;
+  isRedirectBack?: boolean;
 };
 
 export default function ErrorAlert({
@@ -13,6 +15,7 @@ export default function ErrorAlert({
   description,
   href = "/",
   linkValue = "Volver al inicio",
+  isRedirectBack = false,
 }: Props) {
   return (
     <div className="min-h-screen flex justify-center items-center p-6">
@@ -24,11 +27,17 @@ export default function ErrorAlert({
           {description}
         </AlertDescription>
 
-        <Link href={href} className="mt-4">
-          <span className="inline-block w-40 bg-orange-600 text-white px-4 py-2 rounded-md hover:bg-orange-700 transition text-center">
-            {linkValue}
-          </span>
-        </Link>
+        {isRedirectBack ? (
+          <RedirectBack />
+        ) : (
+          <>
+            <Link href={href} className="mt-4">
+              <span className="inline-block w-40 bg-orange-600 text-white px-4 py-2 rounded-md hover:bg-orange-700 transition text-center">
+                {linkValue}
+              </span>
+            </Link>
+          </>
+        )}
       </Alert>
     </div>
   );
