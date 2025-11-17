1f561d6776fbbf4201783b8017e224b5dbc724de
Antonio-Conrado <jconradolopez19@gmail.com>
2025-11-17 11:05:42 -0600

    Added calculateEndDate utility to compute end dates based on payment concepts

---
+++ lib/helpers/calculateEndDate.ts
@@ -0,0 +1,32 @@
+import { Concept } from "@/app/generated/prisma";
+
+export function calculateEndDate(startDate: Date, concept: Concept): Date {
+  const end = new Date(startDate);
+
+  switch (concept) {
+    case Concept.DAY:
+      end.setDate(end.getDate() + 1);
+      break;
+    case Concept.WEEK:
+      end.setDate(end.getDate() + 7);
+      break;
+    case Concept.FORTNIGHT:
+      end.setDate(end.getDate() + 14);
+      break;
+    case Concept.MONTH:
+      end.setMonth(end.getMonth() + 1);
+      break;
+    case Concept.QUARTER:
+      end.setMonth(end.getMonth() + 3);
+      break;
+    case Concept.SEMESTER:
+      end.setMonth(end.getMonth() + 6);
+      break;
+    case Concept.YEAR:
+      end.setFullYear(end.getFullYear() + 1);
+      break;
+    case Concept.OTHER:
+      break;
+  }
+  return end;
+}
