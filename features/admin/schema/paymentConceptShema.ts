86607b185da1a474f0bcad817452f14b936503c9
Antonio-Conrado <jconradolopez19@gmail.com>
2025-11-25 08:09:34 -0600

    Updated amount validation in paymentConceptSchema

--- features/admin/schema/paymentConceptShema.ts
+++ features/admin/schema/paymentConceptShema.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const paymentConceptSchema = z.object({
   amount: z
     .number("El precio del concepto de pago debe ser válido")
-    .min(1, "El precio del concepto de pago debe ser válido"),
+    .min(0, "El precio del concepto de pago debe ser válido"),
   description: z.string().nonempty("La descripción es obligatoria"),
   includedServices: z
     .array(z.string().nonempty("Cada servicio no puede estar vacío"))
