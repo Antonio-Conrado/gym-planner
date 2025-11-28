c5693a4708603532f0ff4672fe3c3f12dfa3e191
Antonio-Conrado <jconradolopez19@gmail.com>
2025-11-27 19:45:57 -0600

    Added formatTimeForInput function and removed UTC timezone from getHour

--- lib/helpers/formatDate.ts
+++ lib/helpers/formatDate.ts
@@ -55,6 +55,12 @@ export function getHour(isoString: string): string {
     hour: "2-digit",
     minute: "2-digit",
     hour12: true,
-    timeZone: "UTC",
   }).format(date);
+}
+
+export function formatTimeForInput(date: Date | null) {
+  if (!date) return "";
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  return `${hours}:${minutes}`;
 }
