9d1a38867d59a817d00506e5a1b8b41baa233a3e
Antonio-Conrado <jconradolopez19@gmail.com>
2025-12-02 09:33:56 -0600

    Set payment start and end dates to start of day (00:00:00) using date-fns

--- lib/helpers/calculateEndDate.ts
+++ lib/helpers/calculateEndDate.ts
@@ -1,33 +1,33 @@
 import { Concept } from "@/app/generated/prisma";
 import { startOfDay } from "date-fns";
 
-export function calculateEndDate(startDate: Date, concept: Concept): Date {
-  const end = startOfDay(startDate);
+export function calculateendDateDate(startDate: Date, concept: Concept): Date {
+  const endDate = startOfDay(startDate);
 
   switch (concept) {
     case Concept.DAY:
-      end.setDate(end.getDate() + 1);
+      endDate.setDate(endDate.getDate() + 1);
       break;
     case Concept.WEEK:
-      end.setDate(end.getDate() + 7);
+      endDate.setDate(endDate.getDate() + 7);
       break;
     case Concept.FORTNIGHT:
-      end.setDate(end.getDate() + 14);
+      endDate.setDate(endDate.getDate() + 14);
       break;
     case Concept.MONTH:
-      end.setMonth(end.getMonth() + 1);
+      endDate.setMonth(endDate.getMonth() + 1);
       break;
     case Concept.QUARTER:
-      end.setMonth(end.getMonth() + 3);
+      endDate.setMonth(endDate.getMonth() + 3);
       break;
     case Concept.SEMESTER:
-      end.setMonth(end.getMonth() + 6);
+      endDate.setMonth(endDate.getMonth() + 6);
       break;
     case Concept.YEAR:
-      end.setFullYear(end.getFullYear() + 1);
+      endDate.setFullYear(endDate.getFullYear() + 1);
       break;
     case Concept.OTHER:
       break;
   }
-  return startOfDay(end);
+  return startOfDay(endDate);
 }
